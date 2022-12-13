e00c4f3ecf9372c77beabba7880fc18b56b2caad
m.yuki <c0a2116813@edu.teu.ac.jp>
2022-12-13 16:30:43 +0900

    コメント

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,6 +1,8 @@
 import pygame as pg
 import random
 import sys
+import tkinter.messagebox as tkm
+import tkinter as tk
 
 
 def check_bound(obj_rct, scr_rct):
@@ -12,6 +14,7 @@ def check_bound(obj_rct, scr_rct):
         yoko = -1
     if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
         tate = -1
+
     return yoko, tate
 
 def main():
@@ -31,6 +34,13 @@ def main():
     # scrn_sfcにtori_rctに従って，tori_sfcを貼り付ける
     scrn_sfc.blit(tori_sfc, tori_rct) 
 
+    tori_sfc2 = pg.image.load("fig/5.png")
+    tori_sfc2 = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct2 = tori_sfc.get_rect()
+    tori_rct2.center = 700, 400
+    # scrn_sfcにtori_rctに従って，tori_sfcを貼り付ける
+    scrn_sfc.blit(tori_sfc2, tori_rct2)
+
     # 練習５
     bomb_sfc = pg.Surface((20, 20)) # 正方形の空のSurface
     bomb_sfc.set_colorkey((0, 0, 0))
@@ -40,6 +50,25 @@ def main():
     bomb_rct.centery = random.randint(0, scrn_rct.height)
     scrn_sfc.blit(bomb_sfc, bomb_rct) 
     vx, vy = +1, +1
+
+    #爆弾を増やす
+    bomb_sfc2 = pg.Surface((20, 20)) # 正方形の空のSurface
+    bomb_sfc2.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc2, (255, 0, 255), (30, 30), 30)
+    bomb_rct2 = bomb_sfc2.get_rect()
+    bomb_rct2.centerx = random.randint(0, scrn_rct.width)
+    bomb_rct2.centery = random.randint(0, scrn_rct.height)
+    scrn_sfc.blit(bomb_sfc2, bomb_rct2) 
+    vx2, vy2 = +1, +1
+
+    bomb_sfc3 = pg.Surface((20, 20)) # 正方形の空のSurface
+    bomb_sfc3.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc3, (255, 255, 0), (50, 50), 50)
+    bomb_rct3 = bomb_sfc3.get_rect()
+    bomb_rct2.centerx = random.randint(0, scrn_rct.width)
+    bomb_rct2.centery = random.randint(0, scrn_rct.height)
+    scrn_sfc.blit(bomb_sfc3, bomb_rct3) 
+    vx3, vy3 = +1, +1
 
     # 練習２
     while True:
@@ -78,9 +107,36 @@ def main():
         vx *= yoko
         vy *= tate
 
+        #爆弾増やす
+        bomb_rct2.move_ip(vx2, vy2)
+        scrn_sfc.blit(bomb_sfc2, bomb_rct2) 
+        yoko, tate = check_bound(bomb_rct2, scrn_rct)
+        vx2 *= yoko
+        vy2 *= tate
+        #爆弾増やす
+        bomb_rct3.move_ip(vx3, vy3)
+        scrn_sfc.blit(bomb_sfc3, bomb_rct3) 
+        yoko, tate = check_bound(bomb_rct3, scrn_rct)
+        vx3 *= yoko
+        vy3 *= tate
+
         # 練習８
+        #爆弾にあたるとメッセージが出る
         if tori_rct.colliderect(bomb_rct):
-            return
+                    root = tk.Tk()
+                    root.withdraw()
+                    tkm.showinfo("ねえ今どんな気持ち？","Game Over")#コメントを表示
+                    return
+        if tori_rct.colliderect(bomb_rct2):
+                    root = tk.Tk()
+                    root.withdraw()
+                    tkm.showinfo("ねえ今どんな気持ち？","Game Over")#コメントを表示
+                    return
+        if tori_rct.colliderect(bomb_rct2):
+                    root = tk.Tk()
+                    root.withdraw()
+                    tkm.showinfo("ねえ今どんな気持ち？","Game Over")#コメントを表示
+                    return
 
         pg.display.update()
         clock.tick(1000)
